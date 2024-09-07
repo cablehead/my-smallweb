d97a3da4de4c0288d4d0125302b2d95f1ed78dcf
Andy Gayton <andy@thecablelounge.com>
2024-09-07 10:44:34 -0400

    wip

--- example/main.ts
+++ example/main.ts
@@ -1,6 +1,6 @@
 export default {
   fetch() {
-    return new Response("Smallweb is running", {
+    return new Response("example domain:: edit ~/smallweb/example/main.tx to change", {
       headers: {
         "Content-Type": "text/plain",
       },
